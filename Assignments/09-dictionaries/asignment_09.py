02fd614a2a9329ef86a2f0c2e3e4fd6b4b890139
udit <udit90@gmail.com>
2018-01-21 18:59:57 +0530

    assignment-09

---
+++ Assignments/09-dictionaries/asignment_09.py
@@ -0,0 +1,41 @@
+
+
+""" Write a program to read through the mbox-short.txt and figure out who has the sent the greatest
+number of mail messages. The program looks for 'From ' lines and takes the second word of those
+lines as the person who sent the mail. The program creates a Python dictionary that maps the
+sender's mail address to a count of the number of times they appear in the file. After the dictionary
+is produced, the program reads through the dictionary using a maximum loop to find the most prolific
+committer.
+
+Desired output: cwen@iupui.edu 5 """
+
+
+
+#Taking User Input
+filename = raw_input ( " Please enter the File Name ")
+fhandle= open(filename)
+email=''
+x=''
+dictionary=dict()
+count=0
+#Adding all the emails and their respective counts in a dictionary
+for line in fhandle :
+    line=line.strip()
+    if line.find('From')==0:
+     x = line.split()
+
+    temp=x[1]
+    if temp not in dictionary :
+        dictionary[temp]=1
+        continue
+    else :
+        dictionary[temp]+= 1
+        continue
+#finding the maximum value in dictionary
+for email in dictionary :
+    if dictionary[email] > count :
+        temp=email
+        count=dictionary[email]
+
+print ( " Maximum Emails were sent by below mentioned user : ")
+print ( temp,dictionary[temp])
