fc95bf00aba0d004636c11078e5db55a095f6824
kapil-varshney <kvarshney.14@gmail.com>
2018-02-16 14:26:01 +0530

    Graded Ex1, 2

--- hussain/ex_002.1.py
+++ hussain/ex_002.1.py
@@ -16,3 +16,14 @@ if hours <= 40:
   print(hours*rate)
 else :
  print((rate*40)+(hours-40)*rate*1.5)
+
+
+"""
+Remarks:
+1. Have the proper question at the beginning of the program
+2. Be consistent with tabs and spaces
+3. Either use 'exit()' in the except block or have the calculation in try block
+Your program tries to execute even if I enter random characters.
+
+Score: 12/10
+"""
