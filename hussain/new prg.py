ea31f4f77db981ad782c0f4cbb47c3fce352031a
kapil-varshney <kvarshney.14@gmail.com>
2018-02-16 14:30:46 +0530

    Update

--- hussain/new prg.py
+++
@@ -1,9 +0,0 @@
-'''
-input the number from user and to find the number as integer or decimal
-'''
-num= input('please enter a number')
-float(num)
-if (float(num)== int(num)):
-    print(num, 'is integer')
-else:
-    print(num, 'is float')
