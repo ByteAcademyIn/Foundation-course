5b7e6ccf4a78151c198c67e1525c6438b40df627
joseantonyned <joseantonynedunilath@gmail.com>
2018-03-21 14:23:11 +0530

    cleaning up and fixing errors

--- jose_antony/assignment_01.py
+++ jose_antony/assignment_01.py
@@ -1,3 +1,10 @@
+"""
+A program to prompt the user for hours and rate per hour 
+using input to compute gross pay. Use 35 hours and 
+a rate of 2.75 per hour to test the program (the pay should be 96.25). 
+Should use input to read a string and float() to convert the string to a number.
+"""
+
 #prompts the user to enter no. of hours
 hours=input("Please enter the no. of hours: ")
 
