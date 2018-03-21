596c19f54b368e5dd536f7e860d7842ce9da87d2
mashok21 <ashok.21aug@gmail.com>
2018-03-21 17:25:23 +0530

    wages assignment completed

--- mashok/helloworld.py
+++ mashok/helloworld.py
@@ -1,1 +1,5 @@
-print ("Hey this is a comment")
+
+No_of_hours = int(input("Enter number of hours worked: "))
+Hourly_rate = input("Enter hourly wage rate: ")
+Wage_cost = float((No_of_hours*Hourly_rate))
+print('wage cost is', Wage_cost)
