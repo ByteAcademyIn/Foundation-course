6a2f81450378c387241c78ba685d85c70acef6c4
mashok21 <ashok.21aug@gmail.com>
2018-03-22 17:06:46 +0530

    file with try and except revised

--- mashok/ex_03.py
+++ mashok/ex_03.py
@@ -2,21 +2,22 @@
 and above 40 hours the employee is eligible for 1.5 times the normal wages"""
 
 # User inputs
-No_of_hours = input("Enter number of hours worked: ")
-Hourly_rate = input("Enter hourly wage rate: ")
-
+hours = input("Enter number of hours worked: ")
+hourly_rate = input("Enter hourly wage rate: ")
 # Check to validate data type for No_of_hours
 try :
-    Num_No_of_hours = int(No_of_hours)
-    Hourly_rate_float = float(Hourly_rate)
+     n_hours = int(hours)
+# Check to validate data type for hourly wage rate
 except :
-    print("Give me a number please")
-
-# Use an if condition to check whether number of hours worked is greater than 40
-if Num_No_of_hours > 40 :
+    print("Give me a number for hours worked, please")
+try :
+        f_hourly_rate = float(hourly_rate)
+except :
+    print("Give me a number for hourly wage rate, please")
+if n_hours > 40 :
 # Formula
-    Wage_cost = (Num_No_of_hours * Hourly_rate_float) + (Num_No_of_hours - 40) * 0.5 * Hourly_rate_float
+    Wage_cost = (n_hours * f_hourly_rate) + (n_hours - 40) * 0.5 * f_hourly_rate
 else :
 # Formula
-    Wage_cost = (Num_No_of_hours * Hourly_rate_float)
+        Wage_cost = (n_hours * f_hourly_rate)
 print('wage cost is', Wage_cost)
