3735359a5c1cf85f0014840969e9f3cd44885845
joseantonyned <joseantonynedunilath@gmail.com>
2018-03-23 10:55:00 +0530

    commiting the assignments

---
+++ jose/04_functions/assignment_04.py
@@ -0,0 +1,47 @@
+"""
+Code to prompt the user for hours and rate per hour using input
+to compute gross pay. Award time-and-a-half for the hourly rate for all hours
+worked above 40 hours. Put the logic to do the computation of time-and-a-half
+in a function called computepay() and use the function to do the computation.
+The function should return a value. Use 45 hours and a rate of 10.50 per hour
+to test the program (the pay should be 498.75). You should use input to read a
+string and float() to convert the string to a number. Do not worry about error
+checking the user input unless you want to - you can assume the user types
+numbers properly. Do not name your variable sum or use the sum() function.
+
+"""
+# defining function to compute the gross pay for hours more than 40hrs
+def computepay(h,r):
+	payment=(40*r)+((h-40)*r*1.5)
+	return payment
+
+# Prompts the user to enter no. of hours
+hours=input("Please enter the no. of hours : ")
+
+# Handling non-numeric hour value
+try:
+	hour_check = float(hours)
+
+	#prompts the user to enter rate per hour
+	rate=input("Please enter your rate per hour : ")
+
+	# Handling non-numeric rate value
+	try:
+		rate_check = float(rate)
+
+		# Computing the gross pay for hours less than or equal to 40hrs
+		if hour_check<=40:
+			pay=hour_check*rate_check
+
+		# calling function to compute the gross pay for hours more than 40hrs
+		else:
+			pay=computepay(hour_check,rate_check)
+
+		#Prints the gross pay
+		print("Gross pay : ",pay)
+
+	except:
+		print("Error! Please enter a numeric value.")
+
+except:
+	print("Error! Please enter a numeric value.")
