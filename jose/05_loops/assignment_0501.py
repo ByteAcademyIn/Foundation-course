ddd32d05b1d4c543715c057499928818e4aa14ea
joseantonyned <joseantonynedunilath@gmail.com>
2018-03-26 11:55:35 +0530

    commiting loops assignments

--- jose/05_loops/assignment_0501.py
+++ jose/05_loops/assignment_0501.py
@@ -1,9 +1,11 @@
-"""Write a program which repeatedly reads numbers until 
-the user enters "done". Once "done" is entered, print out 
-the total, count, and average of the numbers. If the user 
-enters anything other than a number, detect their mistake 
-using try and except and print an error message and skip 
-to the next number."""
+"""
+Code which repeatedly reads numbers until the user enters "done". 
+Once "done" is entered, print out the total, count, and average 
+of the numbers. If the user enters anything other than a number,
+detect their mistake using try and except and print an error 
+message and skip to the next number.
+
+"""
 
 #initialising total, count and average
 tot=0
@@ -11,23 +13,27 @@ cnt=0
 avg=0
 
 while True:
+
 	#Prompts the user to enter a number or type done:
 	n=(input('Please enter a number or type "done": '))
+
 	#detects "done" breaks the loop
 	if n=="done":
 		break
 	try:
+
 		#Filters non-numeric values
 		num=float(n)
 	except:
-		#Prints the error
+
+		#Prints the error and continues back to loop
 		print("Invalid input.")
-		#continues back to loop
 		continue
 
 	#computes total, count and average
 	tot=tot+num
 	cnt=cnt+1
 	avg=tot/cnt
+
 #prints the result
 print("Total:",tot," Count:",cnt," Average:",avg)
--- jose/05_loops/assignment_0502.py
+++ jose/05_loops/assignment_0502.py
@@ -1,29 +1,33 @@
 """
-Write a program that repeatedly prompts a user for integer numbers 
-until the user enters 'done'. Once 'done' is entered, print out the 
-largest and smallest of the numbers. If the user enters anything 
-other than a valid number catch it with a try/except and put out 
-an appropriate message and ignore the number.
+Code that repeatedly prompts a user for integer numbers until the 
+user enters 'done'. Once 'done' is entered, print out the largest 
+and smallest of the numbers. If the user enters anything other than 
+a valid number catch it with a try/except and put out an appropriate
+message and ignore the number.
+
 """
 
-#initialising large and small
+#initialising largest and smallest with None
 largest=None
 smallest=None
 
 while True:
+
 	#Prompts the user to enter a number or type done:
 	n=(input('Please enter a number or type "done": '))
+
 	#detects "done" breaks the loop
 	if n=="done":
 		break
 	try:
+
 		#Filters non-numeric values
 		large=float(n)
 		small=float(n)
 	except:
-		#Prints the error
+
+		#Prints the error and continues back to loop
 		print("Invalid input.")
-		#continues back to loop
 		continue
 
 	#computes largest and smallest numbers
