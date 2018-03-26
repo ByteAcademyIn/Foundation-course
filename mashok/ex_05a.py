b78bacf2477f12ac01c1a79afec4df45e2f56a99
mashok21 <ashok.21aug@gmail.com>
2018-03-26 16:39:53 +0530

    total average and count assignment

--- mashok/ex_05a.py
+++ mashok/ex_05a.py
@@ -1,7 +1,7 @@
 """ Ask the user to input numbers when prompted. Once the user enters 'done'
 calculate the total, average and count. """
 sum = 0 # initialising a while loop starting with zero
-count = 0 
+count = 0
 
 while True:
     say = input("enter a number: ")
@@ -13,4 +13,4 @@ while True:
         count = count + 1 # a store for the number of inputs
     except ValueError: # creating an exception type for error in value type
         print("invalid data")
-print(sum, count, sum/count)
+print(sum, count, sum/count) # print result
