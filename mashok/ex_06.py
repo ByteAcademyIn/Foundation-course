84d9ff70a3abd4a403788fa4cd324286fe0d3353
mashok21 <ashok.21aug@gmail.com>
2018-03-29 13:17:12 +0530

    slicing assignment

--- mashok/ex_06.py
+++ mashok/ex_06.py
@@ -3,7 +3,11 @@
 
 data = 'X-DSPAM-Confidence: 0.8475'
 
-"""" Using find and moving two spaces to account for the colon and space after
-colon. Then leaving blank the end so that all numbers are extracted """
-
-Numbers = print(float(data[(data.find(':')) + 2 :]))
+""" Using find and moving two spaces to account for the colon and space after
+colon. Then leaving blank the 'end' so that all numbers are extracted """
+atpos = data.find(':')
+print(atpos)
+last_text = data[atpos+1: ]
+print(last_text)
+strip_last_text = last_text.lstrip(' ')
+print(strip_last_text)
